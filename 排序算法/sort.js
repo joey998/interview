30dc0311a4f11daba39b0ddb292e506d38cfa861
zhangjiaming <2273243363@qq.com>
2021-04-10 18:12:37 +0800

    add merge and quick sort

--- 排序算法/sort.js
+++ 排序算法/sort.js
@@ -33,7 +33,7 @@ function shuffle(arr){
 function getShuffleArray(length = 1000){
   let origin = []
   for(let i = 0; i < length; i++) {
-    origin.push(i)
+    origin.push(i)  
   }
   let arr = shuffle(origin)
   // console.log('origin', arr);
@@ -46,7 +46,10 @@ function sortInfo( name = 'sort', fn, length){
   let timeStart = new Date();
   let sortedArr = fn(arr)
   let timeEnd = new Date();
-  console.log(name, 'result: ', sortedArr, ', timeUse(ms):', timeEnd - timeStart);
+  console.log(`---------------${name + ' start'}-------------------------`,
+    '\ntimeUse(ms):', timeEnd - timeStart, '\nresult:\n前20位', sortedArr.slice(0, 20).toString(), "\n后20位", sortedArr.slice(length-20).toString(),
+    `\n---------------------${name + ' end'}-------------------------`
+    );
 }
 
 //冒泡算法(双层循环)
@@ -95,6 +98,87 @@ function insertionSort(arr, compareFn = defaultFn){
   return arr
 }
 
-// sortInfo('bubbleSort', bubbleSort, 100000) //24824
-// sortInfo('selectionSort', selectionSort, 100000) //6337
-sortInfo('insertionSort', insertionSort, 100000) //30948
+// 归并算法（将一个大数组分割为左右两个数组，然后将左右两个数组接着再分，直到分得只有一个元素，然后在排序小数组成大数组）
+
+//分割
+function mergeSort(arr, compareFn = defaultFn) {
+  if(arr.length > 1){
+    let {length} = arr;
+    let middle = Math.floor(length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle);
+    left = mergeSort(left, compareFn = defaultFn)
+    right = mergeSort(right, compareFn = defaultFn)
+    arr = merge(left, right, compareFn)
+  }
+  return arr
+}
+//合并
+function merge(left, right, compareFn){
+  let result = []
+  let i = 0, j = 0;
+  while(i < left.length && j < right.length) {
+    if(left[i] < right[j]){
+      result.push(left[i])
+      i++
+    } else {
+      result.push(right[j])
+      j++
+    } 
+  }
+  return result.concat(i === left.length ? right.slice(j) : left.slice(i))
+}
+
+
+/* 快速排序
+  1. 指定主元，一般为left和right的中间值
+  2. 左指针向右移动，直到碰到值大于或者等于主元的值
+  3. 右指针向左移动，直到碰到值小于或者等于主元的值
+  4. 交换指针所指的两个值
+  5. 重复2，3步，直到左指针索引大于右指针。
+  （此时，在左指针左边的所有值都小于或者等于主元，左指针右边的所有值都大于或者等于主元）
+  6. 左右两边的子数组继续执行123456步骤。
+*/
+function quickSort(arr, compareFn = defaultFn) {
+  return quick(arr, 0, arr.length - 1, compareFn)
+}
+
+function quick(arr, left, right, compareFn){
+  if(arr.length > 1) {
+    let index = partition(arr, left, right, compareFn);
+    if(index - 1 > left){
+      quick(arr, left, index - 1, compareFn)
+    }
+    if(index < right){
+      quick(arr, index, right, compareFn)
+    }
+  }
+  return arr
+}
+
+function partition(arr, left, right, compareFn){
+  let i = left, j = right;
+  let middleIndex = Math.floor((left + right) / 2)
+  // if(right > left) {  //这里不需要是因为上面的quick已经判断了，left肯定是小于right的
+    while(i <= j){
+      while(compareFn(arr[i], arr[middleIndex]) === Compare.LESS_THAN) {
+        i++
+      }
+      while(compareFn(arr[j], arr[middleIndex]) === Compare.BIGGER_THAN) {
+        j--;
+      }
+      if(i <= j){
+        swap(arr, i, j)
+        i++;
+        j--;
+      }
+    }
+  // }
+  return i
+}
+
+// sortInfo('bubbleSort', bubbleSort, 100000) //24824(4800u)  
+// sortInfo('selectionSort', selectionSort, 100000) //6337(4800u)  7816(i5-8500)
+sortInfo('insertionSort', insertionSort, 100000) //30948(4800u)  31605(i5-8500)
+// sortInfo('mergeSort', mergeSort, 100000) //  54(i5-8500)  
+// sortInfo('quickSort', quickSort, 100000) //  31(i5-8500)  
